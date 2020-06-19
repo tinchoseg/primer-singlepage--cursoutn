d00dedc0c424605c79e14684ccac08d73a7eb7f7
martin <53618938+tinchoseg@users.noreply.github.com>
2020-06-19 19:25:15 -0300

    subi los archivos

---
+++ index.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+  <meta name="viewport" content="width=device-width,minimum-scale=1.0,maximum-scale=1.0,initial-scale=1.0,user-scalable=no">
+
+  <title>Black Design</title>
+  <link rel="stylesheet" href="style.css"></link>
+  <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+</head>
+<body>
+  <header>
+    <div id="logo"></div>
+    <nav id="menu_1">
+
+      <ul class="botonera_vert">
+        <li><a href="#home">Home</a></li>
+        <li><a href="#servicio">Servicios</a></li>
+        <li><a href="#casos_de_exito">Casos&nbsp;de&nbsp;éxito</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#blog">Blog</a></li>
+        <li><a href="#contacto">Contacto</a></li>
+      </ul>
+
+    </nav>
+
+  </header>
+  <section id="seccion_prin">
+
+    <section id="home">
+      <div id="img_slide">
+      </div>
+    </section>
+
+    <section id="servicio">
+<nav="botonera_servicios">
+<ul>
+  <li><a href="index.php?img src=img_1s#servicio">hermoso</a></li>
+  <li><a href="index.php?id=social#servicio">social-media</a></li>
+  <li><a href="index.php?id=web#servicio">desarrollo_web</a></li>
+</ul>
+</nav>
+    <div id="img_1s">
+        <div id="info_1s">
+          <p id="headlin">BRANDING</p>
+          <p id="texto_branding">kuyfscuhg </br>liuegcdñilacugñcjg</br> iUADGGLeaiu </p>
+        </div>
+      </div>
+      <div id="img_2s">
+        <div id="info_2s">
+          <p id="headlin2">SOCIAL&nbsp;MEDIA</p>
+          <p id="texto_branding2">kuyfscuhg </br>liuegcdñilacugñcjg</br> iUADGGLeaiu </p>
+        </div>
+      </div>
+      <div id="img_3s">
+        <div id="info_3s">
+          <p id="headlin3">DESARROLLO&nbsp;WEB</p>
+          <p id="texto_branding3">kuyfscuhg </br>liuegcdñilacugñcjg</br> iUADGGLeaiu </p>
+        </div>
+      </div>
+      <article id="txt_slide">
+      </article>
+    </section>
+
+    <section id="casos_de_exito">
+
+      <div id="img_lce"></div>
+
+      <article id="txt_rce"><p>Lorem ipsum dolor sit amet consectetur adipiscing elit facilisis hac suscipit,</br> per venenatis felis augue facilisi mi parturient</br> sodales nam himenaeos ac, etiam pretium tristique pulvinar imperdiet hendrerit</br> turpis euismod sed. Torquent habitasse lobortis fames integer est semper aptent fringilla, sodales cursus ante pretium per porta sem penatibus fusce, tellus malesuada neque orci facilisi ut volutpat. Semper vivamus.</p><article>
+
+        <div id="img_rce"></div>
+
+        <article id="txt_lce">
+          <h1></h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipiscing elit etiam vitae morbi proin sociosqu, et egestas duis condimentum justo lacus nunc orci hendrerit metus ante. Egestas aenean auctor quam facilisis magna vehicula, erat pulvinar lacinia mattis venenatis eu, sem varius massa porttitor habitant. Pulvinar praesent sociosqu sodales cum natoque aptent feugiat libero ligula molestie, rhoncus suscipit diam fames pretium conubia at accumsan scelerisque, convallis orci id enim facilisis in leo senectus interdum.</p> <p>Tincidunt metus mus scelerisque parturient mauris lacus purus, habitant nibh inceptos per accumsan aenean neque, luctus odio ac donec eu eget. Ullamcorper nulla quisque cras odio ac venenatis enim, quis fermentum dignissim sagittis ante tellus vulputate, quam egestas mattis augue consequat nisl. Montes ut tempor enim natoque rhoncus ridiculus sociis integer pharetra, fusce platea penatibus magnis bibendum posuere cum varius magna fermentum, nulla hendrerit dapibus placerat nisl aptent pulvinar dictum parturie.</p>
+          </article>
+
+        </section>
+
+
+
+
+        <section id="portfolio">
+          <div id="img_1p">  </div>
+          <div id="img_2p">  </div>
+          <div id="img_3p">  </div>
+          <div id="img_4p">  </div>
+          <div id="img_5p">  </div>
+          <div id="img_6p">  </div>
+        </section>
+
+      <section id="blog">
+        <div id="img_1b"> </div>
+        <div id="img_2b"> </div>
+        <div id="img_3b"> </div>
+
+           <article id="txt_1b">
+                  <h1>Why do we use it?</h1>
+                <p> It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+            </article>
+
+        <article id="txt_2b">
+          <h1>cskdjhsdfhs hflso</h1>
+          <p>Nam tristique volutpat ligula aliquet dapibus. Vestibulum faucibus feugiat dolor, quis pellentesque lacus aliquam non. Ut ultricies velit eu lorem faucibus posuere. Phasellus nec libero eu metus condimentum euismod. Suspendisse sodales laoreet ullamcorper. Maecenas nec tristique dolor. Fusce ultrices lorem eros, quis feugiat ex ultrices laoreet. Donec mollis nisi at orci pharetra mollis sit amet eget lacus. Cras lectus enim, mattis et dapibus non, consectetur at quam.</p>
+        </article>
+
+
+        <article id="txt_3b">
+          <h1>Lorem ipsum dolor sit amet consectetur</h1>
+          <p> adipiscing elit nec orci id, tortor nunc montes diam semper nascetur egestas nisi praesent, placerat suscipit maecenas ultrices massa nisl fermentum nibh platea. Placerat orci rutrum quisque pharetra auctor augue mi convallis nibh commodo, risus accumsan ultrices rhoncus curabitur libero est tempor vestibulum hendrerit vel, praesent eleifend non sodales class cras lectus porta nisi. Bibendum pellentesque cras eleifend nullam suscipit netus placerat penatibus dignissim fermentum nostra, class cum integer per dictum inceptos vel quisque mus condimentum libero, in turpis posuere tempus vehicula quis aliquam nam magna luctus.</p>
+         </article>
+      </section>
+
+        <section id="contacto">
+          <div id="img_c"></div>
+          <article id="txt_c"><p>Lorem ipsum dolor sit amet consectetur adipiscing elit platea in dapibus commodo, semper litora ridiculus egestas nisl per dictumst tempus placerat montes. Aliquam feugiat ligula netus venenatis aenean laoreet faucibus est, porta sagittis erat et dignissim molestie blandit, donec nunc congue fermentum velit facilisis ultrices.</p></article>
+        </section>
+
+        <footer>
+          <article id="derechos">@copyright 2018 Black Design Agency</article>
+          <nav id="social">
+
+            <ul class="redes">
+              <li><a href="https://www.instagram.com"><img  src="imagenes/instagram.png" alt="" href="instagram"></a></li>
+              <li><a href="https://www.behance.net"><img src="imagenes/behance.png" alt=""></a></li>
+              <li><a href="https://www.linkedin.com"><img src="imagenes/linkedin.png" alt=""></a></li>
+              <li><a href="https://www.facebook.com"><img src="imagenes/facebook2.png" alt=""></a></li>
+              <li><a href="https://www.twitter.com"><img src="imagenes/twitter.png" alt=""></a></li>
+
+            </ul>
+
+          </nav>
+
+
+
+
+
+        </footer>
+
+
+      </body>
+
+
+      </html>
